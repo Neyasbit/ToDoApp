75a9cc0a59b1112eaabf89162afa2a4aeb25cb16
neyasbit <kobzevjob@gmail.com>
2021-03-24 17:06:49 +0700

    integrated navigation components for fragments

---
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -0,0 +1,39 @@
+package com.example.todoapp.fragments.list
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.fragment.findNavController
+import com.example.todoapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ListFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        val floatingActionButton = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+        val listLayout = view.findViewById<ConstraintLayout>(R.id.listLayout).apply {
+            setOnClickListener {
+                findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+            }
+        }
+
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
+}
